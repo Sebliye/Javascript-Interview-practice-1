1e0256e30762288c757da4aac317f1dee52e55e6
Seble Aygoda <saygoda@mum.edu>
2021-04-07 10:20:37 -0500

    string loop

---
+++ string/filter.js
@@ -0,0 +1,9 @@
+// 15.use array filter methods to filter out strings that contain ‘-’ from a string array.
+ 
+ let arr=['this','hi-there','how-did'];
+
+ let filteredarr=arr.filter(val=>val.includes('-'));
+ console.log(typeof(filteredarr));
+
+
+
---
+++ string/loopstring.js
@@ -0,0 +1,5 @@
+  let str="hello";
+
+  for(let i=0;i<str.length;i++){
+       console.log(str[i])
+  }
